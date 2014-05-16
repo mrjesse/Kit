f05b8cc17b6d26b4f82366b5c7e0541d9b902a01
Sander Laarhoven <lesander@users.noreply.github.com>
2014-05-16 15:37:02 +0200

    Create openKit.lua

---
+++ openKit.lua
@@ -0,0 +1,11 @@
+-- OpenKit v1.0 unstable dist. API
+-- Copyright AxTo Inc. Released under the MIT License.
+
+-- Made for ComputerCraft 1.6 and OpenPeripherials Addons.
+-- Use this file in combination with the normal Kit!
+
+-- How to include the API in your file (assuming that this file is called "openKit"):
+-- os.loadAPI("kit")
+-- os.loadAPI("openKit")
+-- Now call a function from this file using:
+-- openKit.functionName()
